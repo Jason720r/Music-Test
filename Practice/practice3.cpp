4d472f0a88b7997cc9c8a2e6db80c237ddae3117
Jason Li <jasonli99193@gmail.com>
2024-01-12 08:21:47 -0600

    practice 3

---
+++ Practice/practice3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }
+        return str1.substr(0, gcd(str1.length(), str2.length()));
+    }
+
+private:
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+};
